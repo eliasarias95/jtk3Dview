44427c2de2e62fe1409cc828ef83689ce8d9c85e
alijohns <alijohns@mymail.mines.edu>
2016-06-16 13:05:18 -0600

    modified:   Run.java 	deleted:    ../../../data/cubeI.rsf 	deleted:    ../../../data/cubeI.rsf@ 	deleted:    ../../../data/cubeS.rsf 	deleted:    ../../../data/cubeS.rsf@

--- bench/src/viewer/Run.java
+++ bench/src/viewer/Run.java
@@ -12,17 +12,17 @@ public class Run {
   public static void main(String[] args) {
     //String filename = "/Users/earias/home/git/jtk3Dview/data/cubeI.rsf@";
     String filename = "/Users/earias/home/git/jtk3Dview/data/walesTraces.rsf@";
-    int n1 = 500;
+    int n1 = 500; 
+    float d1 = 0.8f;
     float f1 = 0.0f;
-    float d1 = 0.2f;
     Sampling s1 = new Sampling(n1,d1,f1);
     int n2 = 730;
-    float d2 = 0.05f;
-    float f2 = 8.5f;
+    float d2 = 0.1f;
+    float f2 = 0.0f;
     Sampling s2 = new Sampling(n2,d2,f2);
     int n3 = 33;
-    float d3 = 0.05f;
-    float f3 = 1.0f;
+    float d3 = 1.0f;
+    float f3 = 0.0f;
     Sampling s3 = new Sampling(n3,d3,f3);
     float[][][] f = Util.read(n1,n2,n3,filename);
 
